82ad9d398a70c4028cb96c448af4c4486fda426b
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-10-25 11:56:51 +0530

    page break added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:3000",
+  },
 });
